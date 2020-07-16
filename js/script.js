56e146b96b64eedfdbab884e48b5e9cb63987146
webcoding_by <webcoding.by@gmail.com>
2020-07-16 11:39:14 +0300

    Task 1

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Task #1
+
+let userName = prompt(`What you Name?`);
+let userHow = prompt(`What ${userName} how are you?`);
+let userEat = prompt(`What did the gentleman regale today?`);
+let userBye = prompt(`"Unfortunately, I am a robot and do not eat, so your human tastes are not close to me. Farewell..."`);
+
+console.log(`
+            - What you Name?
+            - ${userName}
+            - What ${userName} how are you?
+            - ${userHow}
+            - What did the gentleman regale today?
+            - ${userEat}
+            -"Unfortunately, I am a robot and do not eat, so your human tastes are not close to me. Farewell..."
+            - ${userBye}
+            `);
+
+
+
+
+
+
